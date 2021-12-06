66502db85a672bfdbff84cb90bdca8a1b79a45d0
zhangzhi <1523334128@qq.com>
2021-12-06 14:15:13 +0800

    组件的定义及使用

---
+++ Vue/Vue自我练习/helloworld/hello/src/components/School.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="demo">
+        <h2>学校名字：{{name}}</h2>
+        <h2>学校地址：{{address}}</h2>
+        <button @click="showName">点我出好康的</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'School',
+    data() {
+        return {
+            name:'清华大学',
+            address:'北京'
+        }
+    },
+    methods: {
+        showName(){
+            alert(this.name);
+        }
+    },
+}
+</script>
+
+<style>
+    .demo{
+        background: aqua;
+    }
+</style>
---
+++ Vue/Vue自我练习/helloworld/hello/src/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+new Vue({
+    el: '#root',
+    // template: `<App></App>`,
+    // components: {
+    //     App,
+    // }
+    render:h =>h(App)
+})
